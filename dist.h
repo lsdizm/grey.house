53016305e3afdbd97a36d8badf6b63f4bdf7175b
park byoung chul <ezcaretech.park.b.c@DESKTOP-CAV95B9>
2023-07-14 12:53:35 +0900

    변경

--- dist.h
+++ dist.h
@@ -1,4 +1,4 @@
-sudo git -C /home/ubuntu/grey.house pull
+sudo git -C /home/ubuntu/source/grey.house pull
 sudo systemctl stop grey.house.service
 sudo systemctl start grey.house.service
 sudo systemctl status grey.house.service 
